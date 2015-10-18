8f0e85332c3d3543c08ab464f38acd12e0f3c14d
Daithi O Crualaoich <daithi.ocrualaoich@gmail.com>
2015-10-18 12:32:39 +0100

    Scala and Rust implementations for Problem 6.

---
+++ scala/006.scala
@@ -0,0 +1,22 @@
+/*
+ * Sum square difference
+ * =====================
+ * The sum of the squares of the first ten natural numbers is,
+ * 1^2 + 2^2 + ... + 10^2 = 385
+ *
+ * The square of the sum of the first ten natural numbers is,
+ * (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ *
+ * Hence the difference between the sum of the squares of the first ten natural
+ * numbers and the square of the sum is 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred
+ * natural numbers and the square of the sum.
+ */
+
+// Straightforward list comprehensions and sums.
+val sum = (1 to 100).sum
+val sumSquares = (1 to 100).map { n => n * n }.sum
+
+val answer = sum*sum - sumSquares
+println(answer)
